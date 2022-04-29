61b77800588e64e916240bd695645f68a0dd4962
sado <liaoyonglin@shangbanzu.gift>
2022-04-29 11:00:43 +0800

    add go scanner

---
+++ kit/rule/scanner_test.go
@@ -0,0 +1,51 @@
+package rule
+
+import (
+	"fmt"
+	goscanner "go/scanner"
+	"go/token"
+	"strings"
+	"testing"
+	"text/scanner"
+)
+
+func Test_B(t *testing.T) {
+	// src is the input that we want to tokenize.
+	//src := []byte(`a >= 6 && a != "abc" && a.ABC() != 0;; // Euler.`)
+	src := []byte(`func A() int64 { return 1 } // Euler.`)
+
+	// Initialize the scanner.
+	var s goscanner.Scanner
+	fset := token.NewFileSet()                      // positions are relative to fset
+	file := fset.AddFile("", fset.Base(), len(src)) // register input "file"
+	s.Init(file, src, nil /* no error handler */, goscanner.ScanComments)
+
+	// Repeated calls to Scan yield the token sequence found in the input.
+	for {
+		pos, tok, lit := s.Scan()
+		if tok == token.EOF {
+			break
+		}
+		if tok == token.SEMICOLON {
+			fmt.Println("有分号了")
+		}
+		fmt.Printf("pos:%s\t token:%s\t lit:%q\n", fset.Position(pos), tok, lit)
+	}
+}
+
+func Test_A(t *testing.T) {
+	exp := "a >= 6"
+
+	var s scanner.Scanner
+	s.Init(strings.NewReader(exp))
+
+	s.Filename = exp + "\t"
+	s.Error = func(s *scanner.Scanner, msg string) {
+		fmt.Println("Scanner err msg: ", msg)
+	}
+
+	for tok := s.Scan(); tok != scanner.EOF; tok = s.Scan() {
+		sT := s.TokenText()
+		fmt.Println("token text: ", sT)
+	}
+}
