f6183a0cac9fca092cfa842239c17c53b932eedc
sado <sadosado0823@gmail.com>
2023-10-13 18:14:52 +0800

    add kit localcache

---
+++ pkg/syncx/syncx.go
@@ -0,0 +1,17 @@
+package syncx
+
+import (
+	"fmt"
+	"log"
+	"runtime/debug"
+)
+
+func GoSave(fn func()) {
+	defer func() {
+		if p := recover(); p != nil {
+			log.Printf("[ERROR]: %s", fmt.Sprintf("%s\n%s", p, string(debug.Stack())))
+		}
+	}()
+
+	fn()
+}
